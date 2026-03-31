b73420c81406d5cd466cb5bb3bc54f82d1a1fe32
yegor256 <yegor256@users.noreply.github.com>
2026-03-31 19:52:53 +0000

    deploy: cd7e7886473116137dcf0a59385abd713882a94b

---
+++ scripts/make-colors.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2020-2026 Yegor Bugayenko
+# SPDX-License-Identifier: MIT
+
+set -e
+
+input="$1"
+output="$2"
+
+echo '% Auto-generated from _colors.scss' > "$output"
+grep '^\$' "$input" | sed 's/\$\([a-z]*\): #\([0-9a-fA-F]\{6\}\);/\\definecolor{\1}{HTML}{\U\2}/' | \
+    sed 's/\$\([a-z]*\): #\([0-9a-fA-F]\)\([0-9a-fA-F]\)\([0-9a-fA-F]\);/\\definecolor{\1}{HTML}{\U\2\2\3\3\4\4}/' >> "$output"
+
+printf '\\endinput' >> "$output"
